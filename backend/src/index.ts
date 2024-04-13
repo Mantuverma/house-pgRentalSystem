d9f484e3e09c7d42827387470eff443420895081
MANTU KUMAR <mantukumarverma2004@gmail.com>
2024-04-13 11:02:35 +0530

    search bar modified

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,7 @@ import cookieParser from "cookie-parser"
 import path from "path";
 import { v2 as cloudinary } from "cloudinary";
 import hotelRouter from "./route/hotelRouter";
-import seachRoute from "../src/route/searchRoute"
+import { searchRoute } from "./route/searchRoute";
 import morgan from 'morgan';
 
 cloudinary.config({
@@ -31,7 +31,7 @@ const port = process.env.PORT || 8000;
 app.use(express.static(path.join(__dirname, "../../frontend/dist")))
 app.use("/api/user", userRouter)
 app.use("/api/hotel", hotelRouter)
-app.use("/api/search", seachRoute)
+app.use("/api/search", searchRoute)
 dbConnect();
 
 
